0c692df39999fc0eab7947d33d62632785f52b11
singjun <shinsj4653@gmail.com>
2022-08-08 00:44:06 +0900

    Updates

--- gatsby-browser.js
+++
@@ -1,7 +0,0 @@
-/**
- * Implement Gatsby's Browser APIs in this file.
- *
- * See: https://www.gatsbyjs.com/docs/browser-apis/
- */
-import 'prismjs/themes/prism-tomorrow.css'
-// You can delete this file if you're not using it
